0486e42b762a05360d8e0846f272bc3abddbdd4b
Suzannah Barnes <suzannah.barnes@specsavers.com>
2025-06-03 13:40:42 +0100

    debugging and refactoring

--- BlackJackApp/src/main/java/blackjackapp/Dealer.java
+++ BlackJackApp/src/main/java/blackjackapp/Dealer.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 
 public class Dealer extends Hand {
 
-    public void dealInDealer(){
+    public void dealInDealer() {
         createInitialHand();
         ifDealerScoreLessThanOrEqualTo16Hit();
-        }
+    }
 
-    public void ifDealerScoreLessThanOrEqualTo16Hit(){
-        if(getPoints()<=16){
-            System.out.println("Dealer has " + getPoints() + " points.\nDealer has to draw again. ");
+    public void ifDealerScoreLessThanOrEqualTo16Hit() {
+        if (getPoints() <= 16) {
+            System.out.println("Dealer has " + getPoints() +
+                    " points.\nDealer has to draw again. ");
             hit();
         }
     }
